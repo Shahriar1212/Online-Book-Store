58f0ae5d756e2646a00bde2cab9f8c6ab9f87734
Ttasmiyah <tasmyah.t07@gmail.com>
2019-10-07 15:27:31 +0600

    contUct_us and about_us updated
    extra php file contact is made

--- AnyBook/about_us.php
+++ AnyBook/about_us.php
@@ -3,11 +3,31 @@
 <link rel="stylesheet" href="css/about_us_style.css">
 
 <div class="l">
-    your code will go here
-    apni just ei div er vitore kaj korben
-    kothay kothay edit korsen ta github desktop e dekhte paren
-    save koiren
-    ow, eita tw ht docs e ase, abar copy koira replace kori
+    <div class= "container">
+        <div class= "about">
+            <div class= "text">
+            <h1>About Us</h1>
+            <div class= "line"></div>
+            </div>
+        </div>
+        <div class= "content">
+            <p><b>L</b>orem  ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate minima tenetur, 
+            officia laborum, assumenda numquam ex incidunt earum, enim neque explicabo perspiciatis 
+            fugiat amet commodi dolorem repellendus voluptates repudiandae maxime.
+            orem  ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate minima tenetur, 
+            officia laborum, assumenda numquam ex incidunt earum, enim neque explicabo perspiciatis 
+            fugiat amet commodi dolorem repellendus voluptates repudiandae maxime.
+            orem  ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate minima tenetur, 
+            officia laborum, assumenda numquam ex incidunt earum, enim neque explicabo perspiciatis 
+            fugiat amet commodi dolorem repellendus voluptates repudiandae maxime.
+            orem  ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate minima tenetur, 
+            officia laborum, assumenda numquam ex incidunt earum, enim neque explicabo perspiciatis 
+            fugiat amet commodi dolorem repellendus voluptates repudiandae maxime.
+            orem  ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate minima tenetur, 
+            officia laborum, assumenda numquam ex incidunt earum, enim neque explicabo perspiciatis 
+            fugiat amet commodi dolorem repellendus voluptates repudiandae maxime.</p>
+        </div>
+    </div>
 </div>
 
 
--- AnyBook/contuct_us.php
+++ AnyBook/contuct_us.php
@@ -3,7 +3,23 @@
 <link rel="stylesheet" href="css/contuct_us_style.css">
 
 <div class="l">
-    your code will go here
+
+	<div class="main">
+		<div class="info">Hello! We are always ready to serve you :)</div>
+		<form action="contact.php" method="post" name="form" class="form-box">
+			<label for="name">Name</label><br>
+			<input type="text" name="name" class="inp" placeholder="Enter Your Name" required><br>
+			<label for="email">Email</label><br>
+			<input type="email" name="email" class="inp" placeholder="Enter Your Email ID" required><br>
+			<label for="phone">Phone</label><br>
+			<input type="tel" name="phone" class="inp" placeholder="Enter Your Phone Number" required><br>
+			<label for="message">Message</label><br>
+			<textarea name="msg" class="msg-box" placeholder="Enter Your Message Here" required></textarea><br>
+			<input type="submit" name="submit" value="Submit" class="sub-btn">
+		</form>
+	</div>
+
 </div>
+
 
 <?php include("includes/footer.php") ?>
